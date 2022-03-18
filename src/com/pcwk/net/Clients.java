269a7074cc3f453e80f58174673d2aecbe780cc1
prk4224 <prk0487@gmail.com>
2022-03-18 15:01:25 +0900

    Server_Clients

---
+++ src/com/pcwk/net/Clients.java
@@ -0,0 +1,103 @@
+package com.pcwk.net;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Clients {
+
+	public static void main(String[] args) {
+		if(args.length !=1) {
+			System.out.println("대화명을 입력 하세요.");
+			System.exit(0);			
+		}
+
+		String serverIp = "127.0.0.1";
+		int port = 8888;
+		String name = args[0];
+		
+		try {
+			Socket socket=new Socket(serverIp,port);
+			
+			Thread thread01=new Thread(new ClientSender(socket,name) );
+			Thread thread02=new Thread(new ClientReceiver(socket));
+			
+			
+			thread01.start();
+			thread02.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	//Scanner에서 데이터 입력해서 서버로 전송
+	public static class ClientSender extends Thread{
+		Socket socket ;
+		String name;
+		DataOutputStream out;
+		
+		ClientSender(Socket socket,String name){
+			this.socket = socket;
+			this.name = name;
+			
+			try {
+				out = new DataOutputStream ( socket.getOutputStream());
+			} catch (IOException e) {
+				//e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			Scanner scanner=new Scanner(System.in);
+			
+			try {
+				System.out.println("99------------------out-"+out);
+				if(null != out) {//최초 이름 보내기
+					out.writeUTF(name);
+				}
+				
+				while(null !=out) {//채팅하기
+					out.writeUTF("["+name+"] "+scanner.nextLine());
+
+				}
+				
+			}catch(IOException e) {
+				System.out.println("100-----------------------");
+				System.out.println(e.getMessage());				
+			}
+			
+		}
+		
+	}
+	
+	public static class ClientReceiver extends Thread{
+		Socket socket;
+		DataInputStream in;
+		
+		public ClientReceiver(Socket socket){
+			this.socket = socket;
+			try {
+				in=new  DataInputStream( socket.getInputStream() );
+			} catch (IOException e) {
+				
+			}
+			
+		}
+		
+		@Override
+		public void run() {
+			while(null !=in) {
+				try {
+					System.out.println(in.readUTF());
+				} catch (IOException e) {
+				}
+			}
+		}	
+	}
+}
